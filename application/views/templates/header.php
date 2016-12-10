a1cc2819a949b1ecde9b6d39eccdd5535382433d
kronos93 <desconocido_-_93@hotmail.com>
2016-12-09 22:38:20 -0500

    Vista login
    Vista login

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="UTF-8">
         <title><?= $title ?></title>
+        <link rel="icon" type="image/png" href="<?= base_url() ?>icon.png" />
         <!-- Viewport for Responsivity -->
 		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no">
 		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no">
