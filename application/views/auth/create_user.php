7563941579104022b9b89fb6bd63c111cc5791c4
MaxiTec <mtec@oasishoteles.com>
2016-12-27 16:10:49 -0500

    maquetacion usuarios

--- application/views/auth/create_user.php
+++ application/views/auth/create_user.php
@@ -1,57 +1,67 @@
-<h1><?php echo lang('create_user_heading');?></h1>
-<p><?php echo lang('create_user_subheading');?></p>
+<div class="modal-header">
+	<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+	<h4 class="modal-title"><?php echo lang('create_user_heading');?></h4>
+	<p><?php echo lang('create_user_subheading');?></p>
+	<div id="infoMessage"><?php echo $message;?></div>
+</div>
+<div class="modal-body">
+	<?php echo form_open("auth/create_user",['id'=>'ion_addUser']);?>
+		
+		<div class="container-fluid">
+			<div class="row">
+				<div class="form-group col-xs-12 col-sm-12">
+                    <?php $first_name['class'] = 'form-control';?>
+					<?php echo lang('create_user_fname_label', 'first_name',['class'=>'required']);?>
+					<?php echo form_input($first_name);?>
+                </div>
+                <div class="form-group col-xs-12 col-sm-6">
+                 	<?php $last_name['class'] = 'form-control';?>
+                	<?php echo lang('create_user_lname_label', 'last_name',['class'=>'required']);?>
+					<?php echo form_input($last_name);?>
+                </div>
+                <div class="form-group col-xs-12 col-sm-6">
+                	<?php
+						if($identity_column!=='email') {
+								echo '<p>';
+								echo lang('create_user_identity_label', 'identity');
+								echo '<br />';
+								echo form_error('identity');
+								echo form_input($identity);
+								echo '</p>';
+						}
+					?>
+					<?php echo lang('create_user_company_label', 'company');?>
+					<?php $company['class'] = 'form-control';?>
+					<?php echo form_input($company);?>
+                </div>
+                <div class="form-group col-xs-12 col-sm-6">
+                	<?php $email['class'] = 'form-control';?>
+                	<?php echo lang('create_user_email_label', 'email');?>
+					<?php echo form_input($email);?>
+                </div>
+                <div class="form-group col-xs-12 col-sm-6">
+               		<?php $phone['class'] = 'form-control';?>
+                	<?php echo lang('create_user_phone_label', 'phone');?>
+					<?php echo form_input($phone);?>
+                </div>
+                <div class="form-group col-xs-12 col-sm-6">
+                	<?php $password['class'] = 'form-control';?>
+                	<?php echo lang('create_user_password_label', 'password',['class'=>'required']);?>
+					<?php echo form_input($password);?>
+                </div>
+                <div class="form-group col-xs-12 col-sm-6">
+                	<?php $password_confirm['class'] = 'form-control';?>
+                	<?php echo lang('create_user_password_confirm_label', 'password_confirm',['class'=>'required']);?>
+					<?php echo form_input($password_confirm);?>
+                </div>
+			</div>
+		</div>
+		
 
-<div id="infoMessage"><?php echo $message;?></div>
-
-<?php echo form_open("auth/create_user",['id'=>'ion_addUser']);?>
-
-      <p>
-            <?php echo lang('create_user_fname_label', 'first_name');?> <br />
-            <?php echo form_input($first_name);?>
-      </p>
-
-      <p>
-            <?php echo lang('create_user_lname_label', 'last_name');?> <br />
-            <?php echo form_input($last_name);?>
-      </p>
-      
-      <?php
-      if($identity_column!=='email') {
-          echo '<p>';
-          echo lang('create_user_identity_label', 'identity');
-          echo '<br />';
-          echo form_error('identity');
-          echo form_input($identity);
-          echo '</p>';
-      }
-      ?>
-
-      <p>
-            <?php echo lang('create_user_company_label', 'company');?> <br />
-            <?php echo form_input($company);?>
-      </p>
-
-      <p>
-            <?php echo lang('create_user_email_label', 'email');?> <br />
-            <?php echo form_input($email);?>
-      </p>
-
-      <p>
-            <?php echo lang('create_user_phone_label', 'phone');?> <br />
-            <?php echo form_input($phone);?>
-      </p>
-
-      <p>
-            <?php echo lang('create_user_password_label', 'password');?> <br />
-            <?php echo form_input($password);?>
-      </p>
-
-      <p>
-            <?php echo lang('create_user_password_confirm_label', 'password_confirm');?> <br />
-            <?php echo form_input($password_confirm);?>
-      </p>
+	<?php echo form_close();?>
+</div>
+<div class="modal-footer">
+	<p><?php echo form_submit('submit', lang('create_user_submit_btn'),['class'=>'btn btn-success pull-right']);?></p>
+</div>
 
 
-      <p><?php echo form_submit('submit', lang('create_user_submit_btn'));?></p>
-
-<?php echo form_close();?>
--- application/views/auth/deactivate_user.php
+++ application/views/auth/deactivate_user.php
@@ -1,22 +1,25 @@
-
-        <div class="modal-header">
-         <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-					<h4 class="model-title"><?= lang('deactivate_heading')?></h4>	
-        </div>
-        
-        <p><?php echo sprintf(lang('deactivate_subheading'), $user->username);?></p>
-        <?php echo form_open("auth/deactivate/".$user->id);?>
-          OLA KE ACE 
-          <p>
-            <?php echo lang('deactivate_confirm_y_label', 'confirm');?>
-            <input type="radio" name="confirm" value="yes" checked="checked" />
-            <?php echo lang('deactivate_confirm_n_label', 'confirm');?>
-            <input type="radio" name="confirm" value="no" />
-          </p>
-
-          <?php echo form_hidden($csrf); ?>
-          <?php echo form_hidden(array('id'=>$user->id)); ?>
-
-          <p><?php echo form_submit('submit', lang('deactivate_submit_btn'));?></p>
-
-        <?php echo form_close();?>
+<?php echo form_open("auth/deactivate/".$user->id);?>
+	<div class="modal-header">
+		<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+		<h4 class="model-title"><?= lang('deactivate_heading')?></h4>
+		<p class="text-left"><?php echo sprintf(lang('deactivate_subheading'), $user->username);?></p>
+	</div>
+	<div class="modal-body">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-xs-12">
+					<?php echo lang('deactivate_confirm_y_label', 'confirm');?>
+					<input type="radio" name="confirm" value="yes" checked="checked" />
+					<?php echo lang('deactivate_confirm_n_label', 'confirm');?>
+					<input type="radio" name="confirm" value="no" />
+					<?php echo form_hidden($csrf); ?>
+					<?php echo form_hidden(array('id'=>$user->id)); ?>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="modal-footer">
+		<?php echo form_submit('submit', lang('deactivate_submit_btn'),['class'=>'btn btn-info pull-right']);?>
+	</div>
+<?php echo form_close();?>
+	
