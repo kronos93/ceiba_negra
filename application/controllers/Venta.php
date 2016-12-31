e99ab88156b6498e0d89dfb81373143bbaf211b6
kronos93 <desconocido_-_93@hotmail.com>
2016-12-31 02:59:22 -0500

    Adelanto de contrato

--- application/controllers/Venta.php
+++ application/controllers/Venta.php
@@ -11,4 +11,15 @@ class Venta extends CI_Controller {
 		$data['body'] = "venta";
 		$this->load->view('templates/template',$data);        
     }
+    public function prueba() {
+        $fecha_contrato_txt = "el día jueves, 29 de diciembre del año 2016";
+        $nombre_cliente = "Samuel Rojas Too";
+        $manzana = 28;
+        $data = compact(
+                        'fecha_contrato_txt',
+                        'nombre_cliente',
+                        'manzana'
+                        );
+        $this->load->view("./templates/contrato/contrato",$data);
+    }
 }
