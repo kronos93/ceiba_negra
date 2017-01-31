4190a712dc4bfd320f52833ef0c6e03b6be36afb
Samuel <samuel_-_rojas@hotmail.com>
2017-01-31 15:42:57 -0500

    Historial

--- application/models/Venta_model.php
+++ application/models/Venta_model.php
@@ -8,12 +8,15 @@ class Venta_model extends CI_Model {
         $this->load->database();
     }
     public function get($condicion = array()){
-       
+        $this->db->select("{$this->table}.id_venta,
+                           CONCAT(cliente.first_name,' ',cliente.last_name) AS nombre_cliente,
+                           CONCAT(lider.first_name,' ',lider.last_name) AS nombre_lider");
         $this->db->from($this->table);
         if (count($condicion)) {
             $this->db->where($condicion);
         }
-        $this->db->join('users','ventas.id_cliente = users.id','left');
+        $this->db->join('users as cliente','ventas.id_cliente = cliente.id','left');
+        $this->db->join('users as lider','ventas.id_lider = lider.id','left');
         $query = $this->db->get();
         return $query->result();
     }
