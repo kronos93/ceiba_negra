fc8cf43034063f27ffe40c1b3af2a024e9109d43
Max Tec <max.tec92@hotmail.com>
2017-03-12 16:44:59 -0500

    maquetacion reset style

--- application/views/auth/forgot_password.php
+++ application/views/auth/forgot_password.php
@@ -3,6 +3,7 @@
         <section class="forgotPassword__container">
             <article class="forgotPassword__header">
                 <h3>Recuperacion de Cuenta</h3>
+                <hr class="separator">
             </article>
             <article class="forgotPassword__body">
                 <div class="container-fluid">
@@ -20,17 +21,17 @@
                                 <?php echo $message;?>
                             </div>
                             <?= form_open("auth/forgot_password");?>
-                            <div class="col-xs-12 form-group">
-                                <label for="identity">
-                                    <?php echo (($type=='email') ? sprintf(lang('forgot_password_email_label'), $identity_label) : sprintf(lang('forgot_password_identity_label'), $identity_label));?>
-                                </label>
-                                <?php $identity['class'] = 'form-control';?>
-                                <?php echo form_input($identity);?>
-                            </div>
-                            <p>
-                                <?= form_submit('submit', lang('forgot_password_submit_btn'),['class'=>'btn btn-success pull-right']);?>
-                            </p>
-                            <?= form_close();?>
+                                <div class="col-xs-12 form-group">
+                                    <label for="identity">
+                                        <?php echo (($type=='email') ? sprintf(lang('forgot_password_email_label'), $identity_label) : sprintf(lang('forgot_password_identity_label'), $identity_label));?>
+                                    </label>
+                                    <?php $identity['class'] = 'form-control';?>
+                                    <?php echo form_input($identity);?>
+                                </div>
+                                <p>
+                                    <?= form_submit('submit', lang('forgot_password_submit_btn'),['class'=>'btn btn-success pull-right']);?>
+                                </p>
+                                <?= form_close();?>
                         </div>
                     </div>
                 </div>
--- application/views/auth/reset_password.php
+++ application/views/auth/reset_password.php
@@ -1,23 +1,50 @@
-<h1><?php echo lang('reset_password_heading');?></h1>
+<main class="resetPassword">
+    <div class="resetPassword--wrap">
+        <section class="resetPassword__container">
+            <article>
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-xs-12">
+                            <div class="resetPassword__header">
+                                <h3>Cambiar Contraseña</h3>
+                                <hr class="separator">
+                            </div>
+                            <div class="resetPassword__body">
+                                <div class="row">
+                                    <?php echo form_open('auth/reset_password/' . $code);?>
+                                    <div class="form-group col-xs-12">
+                                        <label for="new_password">
+                                            <?php echo sprintf(lang('reset_password_new_password_label'), $min_password_length);?>
+                                        </label>
+                                        <br />
+                                        <?php $new_password['class'] = 'form-control';?>
+                                        <?php echo form_input($new_password);?>
+                                    </div>
+                                    <div class="form-group col-xs-12">
+                                        <label for="">
+                                            <?php echo lang('reset_password_new_password_confirm_label', 'new_password_confirm');?>
+                                        </label>
+                                        <?php $new_password_confirm['class'] = 'form-control';?>
+                                        <?php echo form_input($new_password_confirm);?>
+                                    </div>
+                                    <div class="form-group col-xs-12">
+                                        <?php echo form_input($user_id);?>
+                                        <?php echo form_hidden($csrf); ?>
+                                    </div>
+                                    <div class="form-group col-xs-12">
+                                        <?php echo form_submit('submit', lang('reset_password_submit_btn'),['class'=>'btn btn-success pull-right']);?>
+                                    </div>
+                                    <?php echo form_close();?>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </article>
+        </section>
+    </div>
+</main>
+<!-- <h1><?php echo lang('reset_password_heading');?></h1> -->
 <div id="infoMessage">
     <?php echo $message;?>
 </div>
-<?php echo form_open('auth/reset_password/' . $code);?>
-<p>
-    <label for="new_password">
-        <?php echo sprintf(lang('reset_password_new_password_label'), $min_password_length);?>
-    </label>
-    <br />
-    <?php echo form_input($new_password);?>
-</p>
-<p>
-    <?php echo lang('reset_password_new_password_confirm_label', 'new_password_confirm');?>
-    <br />
-    <?php echo form_input($new_password_confirm);?>
-</p>
-<?php echo form_input($user_id);?>
-<?php echo form_hidden($csrf); ?>
-<p>
-    <?php echo form_submit('submit', lang('reset_password_submit_btn'));?>
-</p>
-<?php echo form_close();?>
