4d1394d523627344677144b7f1cc68ee038e45eb
Max Tec <max.tec92@hotmail.com>
2017-03-19 18:54:26 -0500

    recibos

--- manual/dist/scripts/main.js
+++ manual/dist/scripts/main.js
@@ -11,3 +11,22 @@ for (var i in contador) {
     contador[i].innerHTML = numerador;
     numerador++;
 }
+
+
+
+
+
+$(window).scroll(function(event) {
+    var $returntop = $("#returntop");
+    var scroll = $(window).scrollTop();
+    if (scroll > 300) {
+        $returntop.show("slow");
+    }
+});
+$("#menu").on("click", "a", function(e) {
+    var tag = $(this).attr("href");
+    console.log(tag);
+    $('html, body').animate({
+        scrollTop: $(tag).offset().top
+    }, 2000);
+});
