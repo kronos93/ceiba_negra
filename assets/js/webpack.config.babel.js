8a07568c69ae92ea70216070f55f069baf10aebc
kronos93 <desconocido_-_93@hotmail.com>
2017-08-21 09:45:49 -0500

    Preconfig

---
+++ assets/js/webpack.config.babel.js
@@ -0,0 +1,47 @@
+import { resolve } from 'path';
+import webpack from 'webpack';
+
+export default (env, argv) => {
+    console.log(env);
+    const PRODUCTION = (env) ? env.prod : false;
+    return {
+        externals: {
+            jquery: 'jQuery' //jquery is external and available at the global variable jQuery
+        },
+        context: resolve(__dirname, 'src'),
+        entry: {
+            'main': ['./script.js'],
+        },
+        module: {
+            rules: [{
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    loader: "babel-loader"
+                },
+                {
+                    test: /\.css$/,
+                    use: ['style-loader', 'css-loader']
+                },
+                {
+                    test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+                    use: [{
+                        loader: "url-loader"
+                    }]
+                },
+                //Configuración especial para datatables y archivos.js
+                {
+                    test: /datatables\.net.*\.js$/,
+                    use: [{
+                        loader: 'imports-loader?define=>false'
+                    }]
+                }
+
+            ]
+        },
+        output: {
+            filename: '[name].bundle.js', //Archivo o carpeta + nombre del archivo de salida
+            chunkFilename: '[name].bundle.js',
+            path: resolve(__dirname, 'dist'),
+        }
+    };
+}
